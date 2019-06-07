29cc6827e70c458c62096d4fefe9c0ef40365f81
env-gp <komatu.hiroyuki.k@gmail.com>
2019-06-07 14:59:53 +0900

    application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
+//= require jquery.turbolinks
 //= require rails-ujs
 //= require bootstrap
 //= require activestorage
 //= require turbolinks
-//= require jquery
-//= require jquery_ujs
-//= require jquery.turbolinks
 //= require toastr
 //= require_tree .
